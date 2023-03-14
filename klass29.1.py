b2bff18d2f766698276e52fc23b49b35f83d6d8e
vinny <viknikgon@yandex.ru>
2023-03-14 17:23:28 +0300

    Изображение Марса

---
+++ klass29.1.py
@@ -0,0 +1,23 @@
+from flask import Flask, url_for
+app = Flask(__name__)
+
+
+@app.route('/image_mars')
+def name():
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='img/mars.jpg')}" 
+               alt="здесь должна была быть картинка, но не нашлась">
+                    <h2>Вот она какая, красная планета</h2>
+                  </body>
+                </html>'''
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
